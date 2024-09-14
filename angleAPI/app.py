c080b14de19c10f4c5a1ac8eb876f168109f0dca
Allen Liu <al3544@princeton.edu>
2024-09-14 14:10:04 -0400

    cleaned up code

--- angleAPI/app.py
+++ angleAPI/app.py
@@ -8,10 +8,8 @@ app = Flask(__name__)
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-# Initialize MediaPipe Pose
 pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
-# Global variable to store the current mode
 current_mode = 'knee'
 
 def calculate_angle(a, b, c):
